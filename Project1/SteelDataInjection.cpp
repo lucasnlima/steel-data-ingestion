8b58fe902dc9bc30b7d06368b3e295083350d5db
beatriz inacio <beatriz.oliveira@bwtech.com>
2020-10-04 23:57:09 -0300

    isso ai

--- Project1/SteelDataInjection.cpp
+++ Project1/SteelDataInjection.cpp
@@ -19,6 +19,9 @@ using namespace std;
 string listaMensagens[200];
 int currentIndex = 0;
 
+HANDLE hSemaphoreLVazia;
+HANDLE hSemaphoreLCheia;
+
 DWORD WINAPI CatchProcessData();
 DWORD WINAPI CapturaDeMensagensTipo11();
 DWORD WINAPI CapturaDeMensagensTipo22();
@@ -54,9 +57,9 @@ int main(char args[]) {
 	unsigned int Thread2Id;
 	unsigned int Thread3Id;
 
-	HANDLE hSemaphore;
-	hSemaphore = CreateSemaphore(NULL, 0, MAX_POSICOES, (LPCWSTR)"SemLista");
 
+	hSemaphoreLVazia = CreateSemaphore(NULL, MAX_POSICOES, MAX_POSICOES, (LPCWSTR)"SemListaVazia");
+	hSemaphoreLCheia = CreateSemaphore(NULL, 0, MAX_POSICOES, (LPCWSTR)"SemListaCheia");
 
 	HANDLE h_Thread1 = (HANDLE)_beginthreadex(
 		NULL,
@@ -77,14 +80,14 @@ int main(char args[]) {
 
 	);
 
-	HANDLE h_thread3 = (HANDLE)_beginthreadex(
-		NULL,
-		0,
-		(CAST_FUNCTION)CapturaDeMensagensTipo22,
-		NULL,
-		0,
-		&Thread3Id
-	);
+	//HANDLE h_thread3 = (HANDLE)_beginthreadex(
+	//	NULL,
+	//	0,
+	//	(CAST_FUNCTION)CapturaDeMensagensTipo22,
+	//	NULL,
+	//	0,
+	//	&Thread3Id
+	//);
 
 	char a = 0;
 	cout << "Main task";
@@ -99,10 +102,15 @@ int main(char args[]) {
 DWORD WINAPI CapturaDeMensagensTipo11() {
 	while (true) {
 		for (int i = 0; i < 200; i++) {
-				if (listaMensagens[i].find("/11/")) {
-					cout << "achei aqui, tipo 11\n";
-				}	
-			}
+			WaitForSingleObject(hSemaphoreLCheia, INFINITE);
+				if (listaMensagens[i].find("/11/")) {						
+					cout << "mensagem tipo 11 encontrada. [MENSAGEM]: " << listaMensagens[i] << endl;
+					//Função do processo de display que exibe na tela a
+					//string
+					listaMensagens[i] = "";	
+					ReleaseSemaphore(hSemaphoreLVazia, NULL, NULL);
+				}				
+			}		
 	}
 	return 0;
 }
@@ -110,9 +118,14 @@ DWORD WINAPI CapturaDeMensagensTipo11() {
 DWORD WINAPI CapturaDeMensagensTipo22() {
 	while (true) {
 		for (int i = 0; i < 200; i++) {
+			WaitForSingleObject(hSemaphoreLCheia, INFINITE);
 			if (listaMensagens[i].find("/22/")) {
-				cout << "achei aqui, tipo 22\n";
+				
+				cout << "mensagem tipo 22 encontrada. [MENSAGEM]: " << listaMensagens[i] << endl;
+				listaMensagens[i] = "";
+				ReleaseSemaphore(hSemaphoreLVazia, NULL, NULL);
 			}
+			
 		}
 	}
 	return 0;
@@ -180,11 +193,14 @@ DWORD WINAPI CatchProcessData() {
 				mensagemTipo1.tempo = now;
 
 				msg1 = std::to_string(mensagemTipo1.nseq) + "/" + to_string(mensagemTipo1.tipo) + "/" + to_string(mensagemTipo1.cadeira) + "/" + to_string(mensagemTipo1.gravidade) + "/" + to_string(mensagemTipo1.classe) + "/" + mensagemTipo1.idFoto + "/" + std::to_string(mensagemTipo1.tempo.wHour) + ":" + std::to_string(mensagemTipo1.tempo.wMinute) + ":" + std::to_string(mensagemTipo1.tempo.wSecond) + ":" + std::to_string(mensagemTipo1.tempo.wMilliseconds);
+				
+				WaitForSingleObject(hSemaphoreLVazia, INFINITE);
 				listaMensagens[currentIndex] = msg1;
 				std::cout << '\n' << msg1;				
 				currentNSEQTipo1++;
 				currentIndex++;
 				if (currentIndex == 200) currentIndex = 0;
+				ReleaseSemaphore(hSemaphoreLCheia, NULL, NULL);
 				reference = (double)(rand() % (maior - menor + 1) + menor);
 				tick = clock();
 			}
@@ -199,7 +215,10 @@ DWORD WINAPI CatchProcessData() {
 				mensagemTipo2.tempo = now;
 
 				msg2 = std::to_string(mensagemTipo2.nseq) + "/" + to_string(mensagemTipo2.tipo) + "/" + to_string(mensagemTipo2.cadeira) + "/" + mensagemTipo2.id + "/" + to_string(mensagemTipo2.temp) + "/" + to_string(mensagemTipo2.vel) + "/" + std::to_string(mensagemTipo2.tempo.wHour) + ":" + std::to_string(mensagemTipo2.tempo.wMinute) + ":" + std::to_string(mensagemTipo2.tempo.wSecond) + ":" + std::to_string(mensagemTipo2.tempo.wMilliseconds);
+				
+				
 				listaMensagens[currentIndex] = msg2;
+
 				std::cout << '\n' << msg2;
 				currentNSEQTipo2++;
 				
